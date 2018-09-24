a56e6fe19e5ff27938604dfc66576a069de1ee45
Nitish Kumar <mintu.nitish@gmail.com>
2018-09-25 02:25:40 +0530

    Account Creation
    Signed-off-by: Nitish Kumar <mintu.nitish@gmail.com>

--- public/app.js
+++ public/app.js
@@ -1,67 +1,148 @@
 /*
-* Frontend Javascript File
-* */
+ * Frontend Logic for application
+ *
+ */
 
+// Container for frontend application
 let app = {};
 
+// Config
 app.config = {
     'sessionToken' : false
 };
 
+// AJAX Client (for RESTful API)
 app.client = {};
 
-app.client.request = (headers, path, method, queryStringObject, payload, callback) => {
+// Interface for making API calls
+app.client.request = function(headers,path,method,queryStringObject,payload,callback){
+
+    // Set defaults
     headers = typeof(headers) == 'object' && headers !== null ? headers : {};
     path = typeof(path) == 'string' ? path : '/';
-    method = typeof(method) == 'string' && ['POST', 'GET', 'PUT', 'DELETE'].indexOf(method) ? method.toUpperCase() : 'GET';
+    method = typeof(method) == 'string' && ['POST','GET','PUT','DELETE'].indexOf(method.toUpperCase()) > -1 ? method.toUpperCase() : 'GET';
     queryStringObject = typeof(queryStringObject) == 'object' && queryStringObject !== null ? queryStringObject : {};
     payload = typeof(payload) == 'object' && payload !== null ? payload : {};
     callback = typeof(callback) == 'function' ? callback : false;
 
+    // For each query string parameter sent, add it to the path
     let requestUrl = path+'?';
     let counter = 0;
-
-    for (let key in queryStringObject) {
-        if (queryStringObject.hasOwnProperty(key)) {
+    for(let queryKey in queryStringObject){
+        if(queryStringObject.hasOwnProperty(queryKey)){
             counter++;
-            if (counter > 1) {
-                requestUrl += '&';
+            // If at least one query string parameter has already been added, preprend new ones with an ampersand
+            if(counter > 1){
+                requestUrl+='&';
             }
-            requestUrl += key + '=' + queryStringObject[key];
+            // Add the key and value
+            requestUrl+=queryKey+'='+queryStringObject[queryKey];
         }
     }
 
+    // Form the http request as a JSON type
     let xhr = new XMLHttpRequest();
     xhr.open(method, requestUrl, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Content-type", "application/json");
 
-    for (let key in headers) {
-        if (headers.hasOwnProperty(key)) {
-            xhr.setRequestHeader(key, headers[key]);
+    // For each header sent, add it to the request
+    for(let headerKey in headers){
+        if(headers.hasOwnProperty(headerKey)){
+            xhr.setRequestHeader(headerKey, headers[headerKey]);
         }
     }
 
-    if (app.config.sessionToken) {
+    // If there is a current session token set, add that as a header
+    if(app.config.sessionToken){
         xhr.setRequestHeader("token", app.config.sessionToken.id);
     }
 
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
+    // When the request comes back, handle the response
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == XMLHttpRequest.DONE) {
             let statusCode = xhr.status;
-            let response = xhr.responseText;
-            if (callback) {
-                try {
-                    let parseResponse = JSON.parse(response);
-                    callback(statusCode, parseResponse);
+            let responseReturned = xhr.responseText;
+
+            // Callback if requested
+            if(callback){
+                try{
+                    let parsedResponse = JSON.parse(responseReturned);
+                    callback(statusCode,parsedResponse);
+                } catch(e){
+                    callback(statusCode,false);
                 }
-                catch (e) {
-                    callback(statusCode, false);
-                }
+
             }
         }
-    };
+    }
 
-    let payloadStr = JSON.stringify(payload);
-    xhr.send(payloadStr);
+    // Send the payload as JSON
+    let payloadString = JSON.stringify(payload);
+    xhr.send(payloadString);
+
 };
 
+// Bind the forms
+app.bindForms = function(){
+    document.querySelector("form").addEventListener("submit", function(e){
+
+        // Stop it from submitting
+        e.preventDefault();
+        let formId = this.id;
+        let path = this.action;
+        let method = this.method.toUpperCase();
+
+        // Hide the error message (if it's currently shown due to a previous error)
+        document.querySelector("#"+formId+" .formError").style.display = 'hidden';
+
+        // Turn the inputs into a payload
+        let payload = {};
+        let elements = this.elements;
+        for(let i = 0; i < elements.length; i++){
+            if(elements[i].type !== 'submit'){
+                let valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
+                payload[elements[i].name] = valueOfElement;
+            }
+        }
+
+        // Call the API
+        app.client.request(undefined,path,method,undefined,payload,function(statusCode,responsePayload){
+            // Display an error on the form if needed
+            if(statusCode !== 200){
+
+                // Try to get the error from the api, or set a default error message
+                let error = typeof(responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
+
+                // Set the formError field with the error text
+                document.querySelector("#"+formId+" .formError").innerHTML = error;
+
+                // Show (unhide) the form error field on the form
+                document.querySelector("#"+formId+" .formError").style.display = 'block';
+
+            } else {
+                // If successful, send to form response processor
+                app.formResponseProcessor(formId,payload,responsePayload);
+            }
+
+        });
+    });
+};
+
+// Form response processor
+app.formResponseProcessor = function(formId,requestPayload,responsePayload){
+    let functionToCall = false;
+    if(formId == 'accountCreate'){
+        // @TODO Do something here now that the account has been created successfully
+    }
+};
+
+// Init (bootstrapping)
+app.init = function(){
+    // Bind all form submissions
+    app.bindForms();
+};
+
+// Call the init processes after the window loads
+window.onload = function(){
+    app.init();
+};
