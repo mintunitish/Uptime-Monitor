c4c632cf53b03623a70efcd4bc715fb5e0af02a2
Nitish Kumar <mintu.nitish@gmail.com>
2018-09-30 23:08:44 +0530

    Init
    Signed-off-by: Nitish Kumar <mintu.nitish@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const server = require('./lib/server');
 const worker = require('./lib/worker');
+const cli = require('./lib/cli');
 
 let app = {};
 
 app.init = () => {
     server.init();
     worker.init();
+    setTimeout(() => {
+        cli.init();
+    }, 50);
 };
 
 app.init();
